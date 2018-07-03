0263c692827cfc6d66c5693792a8a44fad596f7b
zxbange <782808539@qq.com>
2018-07-03 16:26:04 +0800

    第5天

--- class4/json序列化.py
+++ class4/json序列化.py
@@ -1,3 +1,5 @@
+#-*- coding:UTF-8 -*-
+
 import json
 
 
@@ -8,13 +10,13 @@ info = {
 }
 
 f = open("test.txt", "w")
+
 # print(json.dumps(info)) 将其变为了字符串
 # f.write(json.dumps(info))
 json.dump(info, f)
 
 
 # info["age"] = 21
-#
 # json.dump(info, f)  # 将会生成两个字典存进取, 但是无法load，会报错，所以建议只dump一次，只load一次
 
 f.close()
